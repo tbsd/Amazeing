08c622b3a3057ad302f019b0b64504e67c188cb8
tbsd <tbsd>
2020-06-03 23:28:36 +0300

    refsctor Placeholder
    Former-commit-id: f38ed0105715b01a8eeeafc923488383c166532f

--- Assets/PlayerPlaceholderScript.cs
+++ Assets/PlayerPlaceholderScript.cs
@@ -11,23 +11,10 @@ public class PlayerPlaceholderScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //SMOOTH
- 
-        //POSITION
         transform.position = Vector3.Lerp(transform.position, Player.transform.position, 0.1f);
- 
         Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
- 
-        //ROTATION
         Quaternion toRotation = Quaternion.FromToRotation(transform.up, gravDirection) * transform.rotation;
         transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 0.1f);
  
     }
- 
- 
-    public void NewPlanet(GameObject newPlanet) {
- 
-        Planet = newPlanet;
-    }
- 
 }
