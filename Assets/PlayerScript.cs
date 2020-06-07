3cc0cd2413d8f27acd501293e0b71307e3cafe83
tbsd <tbsd>
2020-06-07 08:04:20 +0300

    add player controls on cube
    Former-commit-id: 85eeb692f84ea2338df23851001ee3e10851284f

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -12,7 +12,6 @@ public class PlayerScript : MonoBehaviour
     public GameObject PlayerPlaceholder;
     public Joystick playerJoystick;
     public float speed = 0.1f;
-    public float JumpHeight = 1.2f;
  
     private Shape shape;
 	private float rotationSpeed;
@@ -31,7 +30,7 @@ public class PlayerScript : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-		movePlayer(getInput());
+		movePlayer(getVelocity());
  
     }
 	private void movePlayer(Vector3 direction) {
@@ -59,14 +58,96 @@ public class PlayerScript : MonoBehaviour
         transform.RotateAround(Planet.transform.position, -transform.forward, direction.x * rotationSpeed * Time.deltaTime);
         transform.RotateAround(Planet.transform.position, Planet.transform.up, direction.z * rotationSpeed * Time.deltaTime);
     }
-	
+
+    float gravity = 80;
+    bool OnGround = false;
+    float distanceToGround;
+    Vector3 Groundnormal;
 	void boxMove(Vector3 direction) {
-		rb.velocity = direction;
-		// TODO: check edges
+        Vector3 move = getVelocity();
+		rb.velocity = rb.transform.TransformDirection(move.x, 0, -move.z);
+        RaycastHit hit = new RaycastHit();
+        bool hasHit = false;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10)) {
+            hasHit = true;
+            distanceToGround = hit.distance;
+            if (distanceToGround <= 0.2 + transform.localScale.y / 2f) 
+                OnGround = true;
+            else 
+                OnGround = false;
+          if (hit.transform == Planet.transform)
+            Groundnormal = hit.normal;
+          else {
+            Groundnormal = (transform.position - Planet.transform.position).normalized;
+            OnGround = false;
+          }
+        } else {
+          OnGround = false;
+          bool isSideHit = false;
+          if (Physics.Raycast(transform.position, -transform.forward, out hit, 10))
+            isSideHit = true;
+          else if (isSideHit = Physics.Raycast(transform.position, transform.forward, out hit, 10))
+            isSideHit = true;
+          else if (Physics.Raycast(transform.position, transform.right, out hit, 10))
+            isSideHit = true;
+          else if (Physics.Raycast(transform.position, -transform.right, out hit, 10))
+            isSideHit = true;
+          if (isSideHit) {
+              distanceToGround = hit.distance;
+            if (hit.transform == Planet.transform)
+              Groundnormal = hit.normal;
+            else {
+              Groundnormal = (transform.position - Planet.transform.position).normalized;
+              OnGround = false;
+            }
+          Debug.DrawRay(transform.position, (hit.transform.position - transform.position), Color.red, 5f);
+          }
+        }
+        Vector3 gravDirection;
+        if (hasHit)
+          gravDirection = (transform.position - hit.point).normalized;
+        else
+          gravDirection = (transform.position - Planet.transform.position).normalized;
+        if (!OnGround) {
+            rb.AddForce(gravDirection * -gravity);
+        }
+        float angle = Quaternion.Angle(Quaternion.identity, Quaternion.FromToRotation(transform.up, Groundnormal));
+        if (true) {
+          Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+          transform.rotation = toRotation;
+        }
+
+    }
+	
+	private float rotationCounter = -1;
+	private RaycastHit lastHit = new RaycastHit();
+	void boxMoveNoGravity(Vector3 direction) {
+		Vector3 localVelocity = transform.TransformDirection(new Vector3(direction.x, 0, -direction.z));
+        rb.velocity = new Vector3(localVelocity.x, localVelocity.y, localVelocity.z);
+		// when player reached an edge
+		if (rotationCounter <= 0) {
+            RaycastHit hit = new RaycastHit();
+            if (!Physics.Raycast(transform.position, -transform.up, out hit, 1) || hit.transform.gameObject != Planet) {
+                // get input to find player movement direction
+                Vector3 input = getInput();
+                Vector3 rotateDirection; 
+				Debug.Log(input);
+                if (Mathf.Abs(input.x) < Mathf.Abs(input.z))
+                    rotateDirection = new Vector3(Mathf.Sign(input.x), 0, 0);
+                else
+                    rotateDirection = new Vector3(0, 0, Mathf.Sign(input.z));
+                transform.RotateAround(lastHit.point, rotateDirection, 90);
+            } else if (hit.transform.gameObject == Planet) {
+				lastHit = hit;
+			}
+            Debug.DrawLine(transform.position, hit.point, Color.red, 10f);
+        } else {
+            rotationCounter -= Time.deltaTime;
+        }
 	}
 	
 	// use this instead of standard collision system because it doesn't work
-	// propertly on curved surfaces
+	// propertly on curved surfaces with small objects
 	Vector3 wallsEvasion(Vector3 direction) {
 		float distance = 0.3f;
 		Vector3 result = direction;
@@ -105,20 +186,24 @@ public class PlayerScript : MonoBehaviour
             }
 		return result;
     }
-	
 
-    Vector3 getInput() {
+    Vector3 getVelocity() {
+		Vector3 input = getInput();
         float x = 0;
         float z = 0;
-        if (playerJoystick.Horizontal >= 0.2f)
+        if (input.x >= 0.2f)
           x = speed;
-        if (playerJoystick.Vertical >= 0.2f)
-          z = -speed;
-        if (playerJoystick.Horizontal <= -0.2f)
-          x = -speed;
-        if (playerJoystick.Vertical <= -0.2f)
+        if (input.z >= 0.2f)
           z = speed;
+        if (input.x <= -0.2f)
+          x = -speed;
+        if (input.z <= -0.2f)
+          z = -speed;
         return new Vector3(x, 0, z);
+    }
+
+    Vector3 getInput() {
+        return new Vector3(playerJoystick.Horizontal, 0, -playerJoystick.Vertical);
     }
 
 	void OnCollisionEnter(Collision col) {
